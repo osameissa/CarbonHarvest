4632af02d49226ddf12e79227c025ddedab9ce68
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-15 19:19:53 +0100

    commit

--- carboncapture/src/App.js
+++ carboncapture/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Navbar.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div>
       <header></header>
-      <body className = "home">
+      <body className="home">
         <Navbar />
-        <h1>Carbon Harvest 🌳</h1>
-        <h2>Empowering Farmers 🚜</h2>
-        <h2>Enriching Earth 🌍</h2>
+        <div className="homeContainer">
+          <h1>Carbon Harvest 🌳</h1>
+          <h2>Empowering Farmers 🚜</h2>
+          <h2>Enriching Earth 🌍</h2>
+        </div>
       </body>
     </div>
   );
