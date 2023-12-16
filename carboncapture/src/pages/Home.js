fdccc6976ffb7dd7f0be325bccd879902fc25d3c
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-16 16:18:59 +0100

    commit

---
+++ carboncapture/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react"; // Make sure to import React at the beginning
+import Navbar from "../components/Navbar.js";
+
+function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="homeContainer">
+        <h1>Carbon Harvest 🌳</h1>
+        <h2>Empowering Farmers &nbsp;&nbsp;🚜</h2>
+        <h2>Enriching Earth &nbsp;&nbsp;🌍</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
