eb4d885383936fc554cab6d451a5222055bf3f91
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-16 16:18:48 +0100

    commit

--- carboncapture/src/components/Navbar.js
+++ carboncapture/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -6,13 +7,13 @@ function Navbar() {
       <nav>
         <ul>
           <li>
-            <a href="">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
