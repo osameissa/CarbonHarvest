81db2a8eef1f6bd4bf396e9f68299a7e2a5861b5
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-19 00:03:11 +0100

    commit

---
+++ carboncapture/src/components/Cc.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cc = () => {
+  return (
+    <div className="cc">
+      Made by:&#160;{" "}
+      <a href="https://github.com/osameissa" title="Github">
+        Osame Issa
+      </a>
+    </div>
+  );
+};
+
+export default Cc;
