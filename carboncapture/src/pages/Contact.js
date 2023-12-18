9353181e5dbe299c88e29e6fa1c0c855817ecc9b
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-18 22:32:34 +0100

    commit

--- carboncapture/src/pages/Contact.js
+++ carboncapture/src/pages/Contact.js
@@ -6,13 +6,12 @@ function Contact() {
   return (
     <div class="contactPage">
       <Navbar />
-      <div class="contactContainer">
+      <div class="container">
         <h1>Contact Us 📞 </h1>
         <div>
-          <h2 class="contact-underline">Contact Information</h2>
-          <p>Email: contact@example.com</p>
-          <p>Phone: (123) 456-7890</p>
-          <p>Address: 123 Main Street, Cityville, State, 12345</p>
+          <h3>Email: contact@example.com</h3>
+          <h3>Phone: (123) 456-7890</h3>
+          <h3>Address: 123 Street, 123 45</h3>
         </div>
       </div>
     </div>
