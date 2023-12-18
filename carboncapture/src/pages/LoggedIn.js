5741bf555f0c50fb95c5e7ea39f6d0b7d57559a0
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-18 16:52:38 +0100

    commit

---
+++ carboncapture/src/pages/LoggedIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar.js";
+
+function LoggedIn() {
+  return (
+    <div class="loggedInPage">
+      <Navbar />
+      <div class="loggedInContainer">
+        <h1>Welcome, </h1>
+        <h2>Click here</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
