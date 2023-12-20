7dc7e9c67df8dbde53c9e1e221083e21c5d360e9
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-20 19:31:03 +0100

    commit

--- carboncapture/src/components/LoggedInBar.js
+++ carboncapture/src/components/LoggedInBar.js
@@ -1,21 +1,43 @@
 // LoggedInBar.js
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWallet } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import Web3 from "web3";
+import { useNavigate } from "react-router-dom";
 
-const LoggedInBar = ({ handleLogout, handleAnotherAction }) => {
+const LoggedInBar = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+
+        // Disconnect MetaMask by requesting accounts
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        // Route back to the home page
+        navigate("/");
+      } else {
+        console.error("MetaMask not installed");
+      }
+    } catch (error) {
+      console.error("Error during sign-out:", error);
+    }
+  };
+
   return (
     <div className="navbar">
       <nav>
         {/* Add any navigation items if needed */}
       </nav>
       <div className="connect-button">
-        <button className="walletBtn">
-          <FontAwesomeIcon icon={faWallet} />
+        <button className="signOutBtn" onClick={handleSignOut}>
+          <FontAwesomeIcon icon={faRightFromBracket} />
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default LoggedInBar;
