d2930a1398990fa2286ec7f66268028b6530c039
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-20 02:37:26 +0100

    commit

--- carboncapture/src/App.js
+++ carboncapture/src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 import Home from "./pages/Home.js";
 import About from "./pages/About";
 import Contact from "./pages/Contact.js";
+import LoggedIn from "./pages/LoggedIn"; // Import the LoggedIn component
 
 function App() {
-  document.title = "CarbonHarvest";
+  document.title = "Carbon Harvest";
   return (
     <Router>
       <div>
         <header></header>
         <main>
           <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/loggedIn" element={<LoggedIn />} /> {/* Add this line */}
           </Routes>
         </main>
       </div>
