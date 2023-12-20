c4548a42eb4102d114018db8761071c876a99563
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-20 02:37:34 +0100

    commit

--- carboncapture/src/components/Navbar.js
+++ carboncapture/src/components/Navbar.js
@@ -1,8 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Web3 from "web3";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate(); // useNavigate replaces useHistory in React Router v6
+
+  const connectToMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        // Redirect to the LoggedIn component
+        navigate("/loggedIn");
+      } catch (error) {
+        console.error("Error connecting to MetaMask:", error.message);
+      }
+    } else {
+      console.error("MetaMask not installed");
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          navigate("/loggedIn");
+        } else {
+          navigate("/"); // Redirect to the home page if the user disconnects the wallet
+        }
+      });
+    }
+  }, [navigate]);
+
   return (
     <div className="navbar">
       <nav>
@@ -19,7 +47,7 @@ function Navbar() {
         </ul>
       </nav>
       <div className="connect-button">
-        <button>Connect</button>
+        <button onClick={connectToMetaMask}>Connect</button>
       </div>
     </div>
   );
