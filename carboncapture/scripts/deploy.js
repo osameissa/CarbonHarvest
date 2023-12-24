6c357cb6c8c5464cf241e4b4c33efa4638a754cb
osameissa <113057277+osameissa@users.noreply.github.com>
2023-12-24 23:57:47 +0100

    commit deploy.js

---
+++ carboncapture/scripts/deploy.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying ERC20Token...");
+  const ERC20Token = await ethers.getContractFactory("YieldToken");
+  const erc20Token = await ERC20Token.deploy();
+  await erc20Token.deployed();
+  console.log("ERC20Token deployed to:", erc20Token.address);
+
+  console.log("Deploying NFT...");
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy("MyNFT", "NFT", erc20Token.address);
+  await nft.deployed();
+  console.log("NFT deployed to:", nft.address);
+
+  // Mint example tokens (optional)
+  await nft.mint(deployer.address);
+  console.log("Token minted to:", deployer.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
