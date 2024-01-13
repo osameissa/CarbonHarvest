132fe966c9f838d934e59a89374977572b78bd61
osameissa <113057277+osameissa@users.noreply.github.com>
2024-01-14 00:39:27 +0100

    commit

--- carboncapture/scripts/deploy.js
+++ carboncapture/scripts/deploy.js
@@ -1,26 +1,39 @@
-const { ethers } = require("hardhat");
+const { Web3 } = require("web3");
+const MyERC20ABI =
+  require("../artifacts/contracts/MyERC20.sol/MyERC20.json").abi;
+const MyERC721ABI =
+  require("../artifacts/contracts/MyERC721.sol/MyERC721.json").abi;
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
+async function deploy() {
+  const deployerPrivateKey =
+    "9911d9412911b0ef4830133682b21a143b8904c865e29b6c99b041efcfee2a7f";
 
-  console.log("Deploying ERC20Token...");
-  const ERC20Token = await ethers.getContractFactory("YieldToken");
-  const erc20Token = await ERC20Token.deploy();
-  await erc20Token.deployed();
-  console.log("ERC20Token deployed to:", erc20Token.address);
+  // Connect to the Ethereum network using the private key
+  const provider = new Web3("https://rpc.ankr.com/polygon_mumbai");
 
-  console.log("Deploying NFT...");
-  const NFT = await ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy("MyNFT", "NFT", erc20Token.address);
-  await nft.deployed();
-  console.log("NFT deployed to:", nft.address);
-}
+  // Get the deployer's address
+  const accounts = await provider.eth.getAccounts();
+  const deployerAddress = 0xf0a0fcccfea76bc05602a3d41f3a1c08d7ef6a3e;
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+  // Deploy MyERC20 with the specified address as the owner
+  const MyERC20Factory = new provider.eth.Contract(MyERC20ABI);
+  console.log("MyERC20Factory ABI:", MyERC20Factory.options.jsonInterface);
+  const MyERC20 = await MyERC20Factory.deploy({
+    data: MyERC20ABI.bytecode,
+    arguments: ["0xf0a0fcccfea76bc05602a3d41f3a1c08d7ef6a3e"], // Pass the specified address as the initial owner
+  }).send({
+    from: deployerAddress,
+    gas: "5000000",
   });
 
-  // memento mori, veritas vencit
+  // Deploy MyERC721 with the specified address as the owner
+  await MyERC721.methods.mint(deployerAddress, amountToMint).send({
+    from: deployerAddress,
+    gas: "5000000",
+  });
+
+  console.log("MyERC20 deployed to:", MyERC20.options.address);
+  console.log("MyERC721 deployed to:", MyERC721.options.address);
+}
+
+deploy();
