889f4899eedb528040d4829e156af5d7c86b62be
osameissa <113057277+osameissa@users.noreply.github.com>
2024-01-17 16:25:52 +0100

    commit

--- carboncapture/contracts/~MyERC20.sol
+++ carboncapture/contracts/~MyERC20.sol
@@ -5,10 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyERC20 is ERC20, Ownable {
-    address private erc721Contract;  // Address of the MyERC721 contract
+    address private erc721Contract; 
 
     constructor(address initialOwner) Ownable(initialOwner) ERC20("CarbonCreditToken", "CCT") {
-        // Additional constructor logic if needed
     }
 
     function setERC721Contract(address _erc721Contract) external onlyOwner {
