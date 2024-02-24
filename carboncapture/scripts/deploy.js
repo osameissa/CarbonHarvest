77adae2aa8a0537bcd9fcf2e20c0ab202788c689
osameissa <113057277+osameissa@users.noreply.github.com>
2024-02-24 22:22:10 +0100

    FFS!!!

--- carboncapture/scripts/deploy.js
+++ carboncapture/scripts/deploy.js
@@ -6,14 +6,14 @@ const MyERC721ABI =
 
 async function deploy() {
   const deployerPrivateKey =
-    "9911d9412911b0ef4830133682b21a143b8904c865e29b6c99b041efcfee2a7f";
+    "";
 
   // Connect to the Ethereum network using the private key
   const provider = new Web3("https://rpc.ankr.com/polygon_mumbai");
 
   // Get the deployer's address
   const accounts = await provider.eth.getAccounts();
-  const deployerAddress = ;
+  const deployerAddress = 0xf0a0fcccfea76bc05602a3d41f3a1c08d7ef6a3e;
 
   // Deploy MyERC20 with the specified address as the owner
   const MyERC20Factory = new provider.eth.Contract(MyERC20ABI);
